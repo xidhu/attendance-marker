762e4eeac5dbd50558fc2abf25b47944a9062e5f
xidhu <sidhu3612@gmail.com>
2020-12-02 18:24:54 +0530

    version1

--- data.js
+++ data.js
@@ -10,16 +10,14 @@ function setCookie(name, value, days) {
     ) + expires + "; path=/";
 }
 
-function getCookie(name) { // Split cookie string and get all individual name=value pairs in an array
+function getCookie(name) {
     var cookieArr = document.cookie.split(";");
 
-    // Loop through the array elements
     for (var i = 0; i < cookieArr.length; i++) {
         var cookiePair = cookieArr[i].split("=");
 
-        /* Removing whitespace at the beginning of the cookie name
-        and compare it with the given string */
-        if (name == cookiePair[0].trim()) { // Decode the cookie value and return
+       
+        if (name == cookiePair[0].trim()) { 
             return cookiePair[1];
         }
     }
