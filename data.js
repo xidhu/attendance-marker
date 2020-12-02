6be87f0534e9bb68c55d14d17c0561076b705a07
xidhu <sidhu3612@gmail.com>
2020-12-02 16:19:50 +0530

    ver 1.0

---
+++ data.js
@@ -0,0 +1,37 @@
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    // Split cookie string and get all individual name=value pairs in an array
+    var cookieArr = document.cookie.split(";");
+    
+    // Loop through the array elements
+    for(var i = 0; i < cookieArr.length; i++) {
+        var cookiePair = cookieArr[i].split("=");
+        
+        /* Removing whitespace at the beginning of the cookie name
+        and compare it with the given string */
+        if(name == cookiePair[0].trim()) {
+            // Decode the cookie value and return
+            return cookiePair[1];
+        }
+    }
+    
+    // Return null if not found
+    return null;
+}
+function setData(name,data){
+
+    setCookie(name,JSON.stringify(data),365);
+}
+function getData(name){
+
+    return JSON.parse(getCookie(name));
+}
---
+++ popup.js
@@ -0,0 +1,34 @@
+
+document.addEventListener('DOMContentLoaded',()=>{
+    let options = getData("attendance_marker");
+    if(options != null){
+    document.getElementById("start_roll").value = options.start;
+    document.getElementById("end_roll").value = options.end;
+    document.getElementById("format").value = options.format;
+    document.getElementById("det").value = options.details;
+    document.getElementById("defaultChecked").checked = options.absent;
+    document.getElementById("defaultChecked1").checked = options.present;
+    }
+    document.querySelector(".take_attendance").addEventListener('click',onClick,true);
+    
+    function onClick(){
+    let first_rn = document.getElementById("start_roll").value;
+    let last_rn = document.getElementById("end_roll").value;
+    let format = document.getElementById("format").value;
+    let details = document.getElementById("det").value;
+    let absent = document.getElementById("defaultChecked").checked;
+    let present = document.getElementById("defaultChecked1").checked;
+    let request = {
+        "start":first_rn,
+        "end":last_rn,
+        "format":format,
+        "details":details,
+        "absent":absent,
+        "present":present
+    }
+    setData("attendance_marker",request);
+        chrome.tabs.query({currentWindow:true,active:true},function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id,request);
+        })
+    }
+},false);
