8b7bc195d0918eb1bc7da64e5d2e9b9d7c273307
xidhu <sidhu3612@gmail.com>
2020-12-14 12:06:35 +0530

    ver 2

--- js/data.js
+++ js/data.js
@@ -1,28 +1,30 @@
 function setCookie(name, value, days) {
-  var expires = "";
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (
+        value || ""
+    ) + expires + "; path=/";
 }
 
 function getCookie(name) {
-  var cookieArr = document.cookie.split(";");
+    var cookieArr = document.cookie.split(";");
 
-  for (var i = 0; i < cookieArr.length; i++) {
-    var cookiePair = cookieArr[i].split("=");
+    for (var i = 0; i < cookieArr.length; i++) {
+        var cookiePair = cookieArr[i].split("=");
 
-    if (name == cookiePair[0].trim()) {
-      return cookiePair[1];
+        if (name == cookiePair[0].trim()) {
+            return cookiePair[1];
+        }
     }
-  }
-  return null;
+    return null;
 }
 function setData(name, data) {
-  setCookie(name, JSON.stringify(data), 365);
+    setCookie(name, JSON.stringify(data), 365);
 }
 function getData(name) {
-  return JSON.parse(getCookie(name));
+    return JSON.parse(getCookie(name));
 }
